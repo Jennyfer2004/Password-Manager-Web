cbd3a23b61ae5811434c18b46cfc92b4231e7484
Jennyfer2004 <jennifer.ss2004@gmail.com>
2025-12-24 00:32:37 -0500

    add the principal models

---
+++ password_manager/manager/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+from django.contrib.auth.models import User
+
+class PasswordEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    website = models.CharField(max_length=255)
+    encrypted_password = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.website} para {self.user.username}"
+    
