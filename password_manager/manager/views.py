a7f1e029585b7ccb9afa9c37defa3cba06ffed51
Jennyfer2004 <jennifer.ss2004@gmail.com>
2026-01-07 03:10:25 -0500

    add the new views to the verificate email

--- password_manager/manager/views.py
+++ password_manager/manager/views.py
@@ -1,24 +1,89 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.http import JsonResponse  
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from .models import PasswordEntry
+from django.core.mail import EmailMessage
+from .models import PasswordEntry ,Profile
 from .utils import get_cipher
+from .form import CustomUserCreationForm
+from .tokens import account_activation_token 
 import json
+from django.contrib.auth.models import User
 
+def custom_logout(request):
+    logout(request)
+    return redirect('hyome')
 
+# def register_view(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('dashboard')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'manager/register.html', {'form': form})
+def email_sent_view(request):
+    """
+    Muestra una página confirmando que el correo de verificación ha sido enviado.
+    """
+    return render(request, 'manager/email_sent.html')
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('dashboard')
-    else:
-        form = UserCreationForm()
-    return render(request, 'manager/register.html', {'form': form})
+            user = form.save(commit=False)
+            user.is_active = False # El usuario no puede iniciar sesión hasta que verifique
+            user.save()
+            
+            # Crear el perfil vinculado al usuario
+            Profile.objects.create(user=user)
 
+            # Enviar correo de verificación
+            current_site = get_current_site(request)
+            mail_subject = 'Activa tu cuenta en Password Manager'
+            message = render_to_string('manager/acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(mail_subject, message, to=[to_email])
+            email.send()
+            
+            return redirect('email_sent') # Una página que diga "Revisa tu correo"
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'manager/register.html', {'form': form})
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        
+        # Marcar el email como verificado en el perfil
+        profile = user.profile
+        profile.email_verified = True
+        profile.save()
+        
+        login(request, user)
+        return redirect('hyome') # O a donde quieras redirigirlo
+    else:
+        return render(request, 'manager/activation_invalid.html')
+    
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
