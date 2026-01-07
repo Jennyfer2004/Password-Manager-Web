84f3d839bbae51da039cc77ab012c55faf93d84e
Jennyfer2004 <jennifer.ss2004@gmail.com>
2026-01-07 03:09:36 -0500

    add the new urls to the verificate email

--- password_manager/manager/urls.py
+++ password_manager/manager/urls.py
@@ -7,8 +7,12 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('add/', views.add_password_view, name='add_password'),
+    path('logout/', views.custom_logout, name='logout'),
     path('', views.home_view, name='hyome'),
     path('get-password/<int:entry_id>/', views.get_password_view, name='get_password'),
     path('edit/<int:entry_id>/', views.edit_password_view, name='edit_password'),
     path('delete/<int:entry_id>/', views.delete_password_view, name='delete_password'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('registro-exitoso/', views.email_sent_view, name='email_sent'),
+
 ]
